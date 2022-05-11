cf22d300fa5b9d2c5d5fc53b3eae56e6de9902f5
Mitchell Painter <mitchell.painter@hotmail.com>
2022-05-11 14:29:56 +1000

    Basic CRUD layout complete

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,7 @@
 class BikesController < ApplicationController
+  #DEBUG - REMOVE FOR PRODUCTION!!!
+  skip_before_action :verify_authenticity_token
+
     def index
         @bikes = Bike.all
     end
@@ -9,5 +12,28 @@ class BikesController < ApplicationController
     rescue
       render plain: 'No match found!'
     end
+  end
+
+  def create
+    bike = Bike.create(bike_params)
+    redirect_to bike
+  end
+
+  def update 
+    bike = Bike.find(params[:id]) #, :brand_id, :material_id, :discription, :condition, :bike_type, :size)
+    bike.update(bike_params)
+    redirect_to bike
+  end
+
+  def destroy
+    bike = Bike.find(params[:id])
+    bike.destroy
+    redirect_to bikes_path
   end 
+
+  private
+
+  def bike_params
+    return params.permit(:name, :brand_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     get 'bikes', to: 'bikes#index'
     get 'bikes/:id', to: 'bikes#show', as: 'bike'
+    put 'bikes/:id', to: 'bikes#update'
+    post 'bikes', to: 'bikes#create'
+    delete 'bikes/:id', to: 'bikes#destroy'
     root 'bikes#index'
+    
 end
