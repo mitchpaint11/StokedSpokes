96b2816f27ba9063af3cb125b7d737ebda4e37bc
Mitchell Painter <mitchell.painter@hotmail.com>
2022-05-11 14:48:38 +1000

    Completed CRUD controllers

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,37 +1,35 @@
 class BikesController < ApplicationController
   #DEBUG - REMOVE FOR PRODUCTION!!!
   skip_before_action :verify_authenticity_token
+  before_action :find_bike, only: [:show, :update, :destroy]
 
     def index
         @bikes = Bike.all
     end
 
     def show
-    begin
-      @bike = Bike.find(params[:id])
-    rescue
-      render plain: 'No match found!'
     end
-  end
 
   def create
     bike = Bike.create(bike_params)
     redirect_to bike
   end
 
-  def update 
-    bike = Bike.find(params[:id]) #, :brand_id, :material_id, :discription, :condition, :bike_type, :size)
-    bike.update(bike_params)
-    redirect_to bike
+  def update
+    @bike.update(bike_params)
+    redirect_to @bike
   end
 
   def destroy
-    bike = Bike.find(params[:id])
-    bike.destroy
+    @bike.destroy
     redirect_to bikes_path
   end 
 
   private
+
+  def find_bike
+    @bike = Bike.find(params[:id])
+  end 
 
   def bike_params
     return params.permit(:name, :brand_id)
