754925c970f4f857ad0aa68ff73486e7399feb09
Mitchell Painter <mitchell.painter@hotmail.com>
2022-05-13 11:36:26 +1000

    Completed new/edit forms and delete

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,9 @@
 class BikesController < ApplicationController
   #DEBUG - REMOVE FOR PRODUCTION!!!
   skip_before_action :verify_authenticity_token
-  before_action :find_bike, only: [:show, :update, :destroy]
+  before_action :find_bike, only: [:show, :update, :destroy, :edit]
+  before_action :find_brands, only: [:new, :edit]
+  before_action :find_materials, only: [:new, :edit]
 
     def index
         @bikes = Bike.all
@@ -11,12 +13,15 @@ class BikesController < ApplicationController
     end
 
     def new
-      @bike = Bike.new
+      @bike = Bike.new 
     end 
 
   def create 
     bike = Bike.create!(bike_params)
     redirect_to bike
+  end
+
+  def edit
   end
 
   def update
@@ -33,7 +38,15 @@ class BikesController < ApplicationController
 
   def find_bike
     @bike = Bike.find(params[:id])
-  end 
+  end
+
+  def find_brands
+    @brands = Brand.order(:brand_name)
+  end
+
+  def find_materials
+    @materials = Material.all
+  end
 
   def bike_params
     return params.require(:bike).permit(:name, :brand_id, :price, :discription, :condition, :bike_type, :size)
