0d5db6ce5ebe9ac8d60620d261c862e0fb5b0be5
Mitchell Painter <mitchell.painter@hotmail.com>
2022-05-16 15:04:04 +1000

    Added .env

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,11 @@ cervelo = Brand.create(brand_name: "Cervelo")
 trek = Brand.create(brand_name: "Trek")
 bianchi = Brand.create(brand_name: "Bianchi")
 
-cervelo.bikes.create(name:"Cervelo", price:3000, discription:" ", condition:"Used-like new", bike_type:"Road", size:51,)
-specialized.bikes.create(name:"Specialized Venge", price:3400, discription:" ", condition:"Used-like new", bike_type:"Road", size:54,)
-bianchi.bikes.create(name:"Bianchi", price:1100, discription:" ", condition:"Used-fair", bike_type:"Commuter", size:51,)
-trek.bikes.create(name:"Trek", price:2500, discription:" ", condition:"Used-like new", bike_type:"MTB", size:56,)
-giant.bikes.create(name:"Giant", price:2000, discription:" ", condition:"Used-fair", bike_type:"MTB", size:53,)
+cervelo.bikes.create(name:"Cervelo", price:3000, discription:" ", condition:"Used-Like New", bike_type:"Road", size:51)
+specialized.bikes.create(name:"Specialized Venge", price:3400, discription:" ", condition:"Used-Like New", bike_type:"Road", size:54)
+bianchi.bikes.create(name:"Bianchi", price:1100, discription:" ", condition:"Used-Good", bike_type:"Gravel", size:51)
+trek.bikes.create(name:"Trek", price:2500, discription:" ", condition:"Used-Like New", bike_type:"MTB", size:56)
+giant.bikes.create(name:"Giant", price:2000, discription:" ", condition:"Used-Fair", bike_type:"MTB", size:53)
 
 puts "Users: #{User.count}"
 puts "Roles: #{Role.count}"
